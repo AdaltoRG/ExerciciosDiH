8265be4180a3976ebd936e912eb2645fe628b3ec
AdaltoRG <111100730+AdaltoRG@users.noreply.github.com>
2022-11-26 20:19:35 -0300

    resolução do exercício 4

--- M1S08/NotificationApp/src/app/app.module.ts
+++ M1S08/NotificationApp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NotificationListComponent } from './notification-list/notification-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotificationListComponent
+    NotificationListComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
