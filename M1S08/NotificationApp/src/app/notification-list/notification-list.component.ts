77c07f153398bf7bad8aa24cd369bef6d3e20bc7
AdaltoRG <111100730+AdaltoRG@users.noreply.github.com>
2022-11-26 21:05:15 -0300

    resolução do exercício 9

--- M1S08/NotificationApp/src/app/notification-list/notification-list.component.ts
+++ M1S08/NotificationApp/src/app/notification-list/notification-list.component.ts
@@ -3,19 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ntap-notification-list',
   templateUrl: './notification-list.component.html',
-  styleUrls: ['./notification-list.component.scss']
+  styleUrls: ['./notification-list.component.scss'],
 })
 export class NotificationListComponent {
-notificacoes=[
-  {
-    titulo: 'título',
-    data: '22/12/2022',
-    subtitulo:'subtítulo'
-  },
-  {
-    titulo:'teste 2',
-    data:'11/11/2025',
-    subtitulo:'subTeste'
+  marcarLido(item:any){
+    item.lido = true;        
   }
-]
+  notificacoes = [
+    {
+      titulo: 'título',
+      data: '22/12/2022',
+      subtitulo: 'subtítulo',
+      lido: false,
+    },
+    {
+      titulo: 'teste 2',
+      data: '11/11/2025',
+      subtitulo: 'subTeste',
+      lido: false,
+    },
+  ];
 }
