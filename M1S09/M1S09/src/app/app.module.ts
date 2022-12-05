5eaba23404ed933a62c1b4b97785fedeaddb9752
AdaltoRG <111100730+AdaltoRG@users.noreply.github.com>
2022-12-04 22:41:25 -0300

    Resolução do exercício 2

--- M1S09/M1S09/src/app/app.module.ts
+++ M1S09/M1S09/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormularioRegistroComponent } from './formulario-registro/formulario-registro.component';
+import { ModalComponent } from './formulario-registro/modal/modal.component';
+import { BotaoComponent } from './formulario-registro/botao/botao.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioRegistroComponent
-  ],
+    FormularioRegistroComponent,
+    ModalComponent,
+    BotaoComponent,],
   imports: [
     BrowserModule
   ],
