c15caab6c59d1b8ae12c29598398f097b52cc24d
AdaltoRG <111100730+AdaltoRG@users.noreply.github.com>
2022-12-04 23:48:25 -0300

    resolução do exercício 6

--- M1S09/M1S09/src/app/serie-favorita/item-card/item-card.component.ts
+++ M1S09/M1S09/src/app/serie-favorita/item-card/item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-card',
@@ -6,12 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-card.component.css'],
 })
 export class ItemCardComponent {
-  personagem: any = {
-    nome: 'Wandinha',
-    imagem:
-      'https://www.ofuxico.com.br/wp-content/uploads/2022/11/wandinha-jenny-ortega-seria-em-poster-serie-netflix.jpg',
-    nomeAtor: 'Jenna Ortega',
-    descricao:
-      'Jenna Marie Ortega é uma atriz norte-americana que ficou conhecida devido aos seus papéis como Harley Diaz em Stuck in the Middle, do Disney Channel e Ellie Alves em You e Wednesday, da Netflix.',
-  };
+  @Input() nome: string = "";
+  @Input() imagem: string = "";
+  @Input() nomeAtor: string = "";
+  @Input() descricao: string = "";
+  
 }
--- M1S09/M1S09/src/app/serie-favorita/serie-favorita.component.ts
+++ M1S09/M1S09/src/app/serie-favorita/serie-favorita.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./serie-favorita.component.css']
 })
 export class SerieFavoritaComponent {
-
+  personagem:any={
+    nome:'Wandinha',
+    imagem:'https://www.ofuxico.com.br/wp-content/uploads/2022/11/wandinha-jenny-ortega-seria-em-poster-serie-netflix.jpg',
+    nomeAtor:'Jenna Ortega',
+    descricao:'Jenna Marie Ortega é uma atriz norte-americana que ficou conhecida devido aos seus papéis como...'
+  }
 }
