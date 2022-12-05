556beb75d478ed6a6c6b50cd68c78ee0f2ba3fda
AdaltoRG <111100730+AdaltoRG@users.noreply.github.com>
2022-12-04 23:36:05 -0300

    Resolução do exercício 5

--- M1S09/M1S09/src/app/serie-favorita/item-card/item-card.component.ts
+++ M1S09/M1S09/src/app/serie-favorita/item-card/item-card.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-item-card',
   templateUrl: './item-card.component.html',
-  styleUrls: ['./item-card.component.css']
+  styleUrls: ['./item-card.component.css'],
 })
 export class ItemCardComponent {
-
+  personagem: any = {
+    nome: 'Wandinha',
+    imagem:
+      'https://www.ofuxico.com.br/wp-content/uploads/2022/11/wandinha-jenny-ortega-seria-em-poster-serie-netflix.jpg',
+    nomeAtor: 'Jenna Ortega',
+    descricao:
+      'Jenna Marie Ortega é uma atriz norte-americana que ficou conhecida devido aos seus papéis como Harley Diaz em Stuck in the Middle, do Disney Channel e Ellie Alves em You e Wednesday, da Netflix.',
+  };
 }
