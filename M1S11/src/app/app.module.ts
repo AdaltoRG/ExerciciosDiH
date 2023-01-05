6141075e11bd063c53ba2cffdbf329a3b800289e
AdaltoRG <111100730+AdaltoRG@users.noreply.github.com>
2023-01-05 02:50:15 -0300

    M1S11 - Exercício 2

--- M1S11/src/app/app.module.ts
+++ M1S11/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CaptalizePhillipsPipe } from './pipes/captalize-phillips.pipe';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CaptalizePhillipsPipe
+    CaptalizePhillipsPipe,
+    NavComponent
   ],
   imports: [
     BrowserModule,
